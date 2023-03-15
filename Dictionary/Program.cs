518bd77ea171b5a26c23fb66faab1171794e9dd5
Arnaldo Filho <arnalldofilho@gmail.com>
2023-03-14 21:30:12 -0300

    Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,39 @@
+﻿
+Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minas Gerais");
+
+foreach (var item in estados)
+{
+    System.Console.WriteLine($"Chave: {item.Key} Valor: {item.Value}");
+}
+
+System.Console.WriteLine("\n-------------------------------------------");
+
+estados.Remove("SP");
+estados["BA"] = "Bahia - Valor Alterado";
+
+foreach (var item in estados)
+{
+    System.Console.WriteLine($"Chave: {item.Key} Valor: {item.Value}");
+}
+
+System.Console.WriteLine("\n+++++++++++++++++++++++++++++++++++++++++++++");
+
+string chave = "SP";
+System.Console.WriteLine("Verificando o elemento");
+
+if (estados.ContainsKey(chave))
+{
+    System.Console.WriteLine($"Valor existente: {chave}");
+}
+else
+{
+    System.Console.WriteLine($"Valor não existe é seguro adicionar a chave : {chave}");
+}
+
+System.Console.WriteLine("\n=============== Obtendo um valor que já existe ==================");
+
+System.Console.WriteLine(estados["MG"]);
---
+++ Pilha/Program.cs
@@ -0,0 +1,21 @@
+﻿Stack<int> pilha = new Stack<int>();
+
+pilha.Push(4);
+pilha.Push(6);
+pilha.Push(8);
+pilha.Push(10);
+
+foreach(int item in pilha)
+{
+    System.Console.WriteLine(item);
+}
+
+System.Console.WriteLine($"Removendo o elemento do topo {pilha.Pop()}");
+
+// Acresentando item
+pilha.Push(20);
+
+foreach(int item in pilha)
+{
+    System.Console.WriteLine(item);
+}
