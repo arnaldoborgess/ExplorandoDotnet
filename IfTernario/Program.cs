3c28675d2d2a965c343c90b02734d181a3182d5b
Arnaldo Filho <arnalldofilho@gmail.com>
2023-03-16 17:50:45 -0300

    If Ternário

---
+++ IfTernario/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+int numero = 20;
+bool ehPar = false;
+
+// IF Ternario
+
+ehPar = numero % 2 == 0;
+Console.WriteLine($"O número {numero} é " + (ehPar ? "par" : "impar"));
+
+
+
+
+/*
+if (numero % 2 == 0)
+{
+    System.Console.WriteLine($"O número {numero} é par");
+}
+else
+{
+    System.Console.WriteLine($"O número {numero} é impar");
+}
+*/
