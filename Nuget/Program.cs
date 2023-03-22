b0c2f99adce89791dd2d3141f6c0d322ef1376f2
Arnaldo Filho <arnalldofilho@gmail.com>
2023-03-22 16:02:03 -0300

    Escrevendo arquivo Json, Utilizando datetime ISO 8601

--- Nuget/Program.cs
+++ Nuget/Program.cs
@@ -1,8 +1,18 @@
 ﻿using Nuget.Models;
 using Newtonsoft.Json;
 
-Venda V1 = new Venda(1, "Material de Escritório", 25.00M);
+DateTime dataAtual = DateTime.Now;
 
-string serializado = JsonConvert.SerializeObject(V1, Formatting.Indented);
+List<Venda> listaVendas = new List<Venda>();
+
+Venda V1 = new Venda(1, "Material de Escritório", 25.00M, dataAtual);
+Venda V2 = new Venda(1, "Licença de Software", 110.00M, dataAtual);
+
+listaVendas.Add(V1);
+listaVendas.Add(V2);
+
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+File.WriteAllText("Arquivos/vendas.json", serializado); // Ele cria o formato Json ou qualquer formato.
 
 System.Console.WriteLine(serializado);
