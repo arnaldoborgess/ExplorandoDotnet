3062bff0240c32db17f28e4b2de6a3d45372ad2b
Arnaldo Filho <arnalldofilho@gmail.com>
2023-03-22 13:31:55 -0300

    Nuget, json, serialização

---
+++ Nuget/Program.cs
@@ -0,0 +1,8 @@
+﻿using Nuget.Models;
+using Newtonsoft.Json;
+
+Venda V1 = new Venda(1, "Material de Escritório", 25.00M);
+
+string serializado = JsonConvert.SerializeObject(V1, Formatting.Indented);
+
+System.Console.WriteLine(serializado);
