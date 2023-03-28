403a00c52c496ce8566f6463d52c90160d89596e
Arnaldo Filho <Arnaldo Filho>
2023-03-28 20:57:57 -0300

    Tipo Anonimo

---
+++ TiposEspeciais/TiposAnonimos/Program.cs
@@ -0,0 +1,5 @@
+﻿var tipoAnonimo = new { Nome = "Arnaldo", Sobrenome = "Filho", Altura = 1.80 };
+
+System.Console.WriteLine("Nome: " + tipoAnonimo.Nome);
+System.Console.WriteLine("Sobrenome: " + tipoAnonimo.Sobrenome);
+System.Console.WriteLine("altura: " + tipoAnonimo.Altura);
