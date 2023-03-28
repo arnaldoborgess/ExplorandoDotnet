54b34cb969ce88ef154fd5e62dec7ea4f2808c52
Arnaldo Filho <Arnaldo Filho>
2023-03-27 21:17:02 -0300

    Tipos Especiais - Nulu

---
+++ TiposEspeciais/TiposNulos/Program.cs
@@ -0,0 +1,11 @@
+﻿bool? desejaReceberEmail = null;
+
+if (desejaReceberEmail.HasValue && desejaReceberEmail.Value) // hasvalue ver se é diferente de nulo Value é o valor da variável.
+// Se não quiser usar o hasvalue != null
+{
+    System.Console.WriteLine("O usuário optou por receber e-mail.");
+}
+else
+{
+    System.Console.WriteLine("O usuário não respondeu ou optou por não receber.");
+}
